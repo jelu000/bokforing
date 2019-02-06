a672eb854bcbd9f86c0059c5e9f404367287354b
Jens L <jelu000@gmail.com>
2019-02-06 23:41:29 +0100

    La till column för namn till varje konto. La till text på momsen.html

--- momsen.js
+++ momsen.js
@@ -24,32 +24,33 @@ function calculateVer(){
 
                    var heading = new Array();
                    heading[0] = "Konto"
-                   heading[1] = "Debet"
-                   heading[2] = "Kredit"
+                   heading[1] = "Namn"
+                   heading[2] = "Debet"
+                   heading[3] = "Kredit"
 
 
                    var stock = new Array()
 
                     if (depositFloat==0)
-                        stock.push(new Array("1910", cashFloat, "0"));
+                        stock.push(new Array("1910", "Kassa", cashFloat, "0"));
                     else if (incomeFloat==depositFloat)
-                        stock.push(new Array("1930", depositFloat, "0"));
+                        stock.push(new Array("1930", "Företagskonto", depositFloat, "0"));
                     else {
-                        stock.push(new Array("1910", cashFloat, "0"));
-                        stock.push(new Array("1930", depositFloat, "0"));
+                        stock.push(new Array("1910", "Kassa", cashFloat, "0"));
+                        stock.push(new Array("1930", "Företagskonto", depositFloat, "0"));
                     }
 
-                    stock.push(new Array("2611", "0", taxFloat.toFixed(0)));
+                    stock.push(new Array("2611", "Utgående moms 25%", "0", taxFloat.toFixed(0)));
 
                     if (sailsFloat==0)
-                      stock.push(new Array("3041", "0" , workIncFloatExTax.toFixed(0)));
+                      stock.push(new Array("3041", "Försäljning tjänst 25% moms", "0" , workIncFloatExTax.toFixed(0)));
 
                     else if (workIncFloat==0)
-                      stock.push(new Array("3051", "0" , sailsFloatExTax.toFixed(0)));
+                      stock.push(new Array("3051", "Försäljning varor 25% moms", "0" , sailsFloatExTax.toFixed(0)));
 
                     else{
-                      stock.push(new Array("3041", "0" , workIncFloatExTax.toFixed(0)));
-                      stock.push(new Array("3051", "0" , sailsFloatExTax.toFixed(0)));
+                      stock.push(new Array("3041", "Försäljning tjänst 25% moms", "0" , workIncFloatExTax.toFixed(0)));
+                      stock.push(new Array("3051","Försäljning varor 25% moms", "0" , sailsFloatExTax.toFixed(0)));
                       //stock[3] = new Array("3041", "0" , workIncFloatExTax.toFixed(0))
                     }
 
@@ -68,7 +69,14 @@ function calculateVer(){
                     tableJhaed.appendChild(tr);
                     for (i = 0; i < heading.length; i++) {
                         var th = document.createElement('TH')
-                        th.width = '75';
+
+                        if (i==1)
+                          th.width = '220';
+
+
+                        else
+                          th.width = '75';
+
                         th.appendChild(document.createTextNode(heading[i]));
                         tr.appendChild(th);
 
@@ -85,6 +93,10 @@ function calculateVer(){
                         for (j = 0; j < stock[i].length; j++) {
                             console.log(stock[i][j]);
                             var td = document.createElement('TD')
+
+                            if (j==1)
+                              td.align = 'left';
+
                             td.appendChild(document.createTextNode(stock[i][j]));
                             tr.appendChild(td);
                         }
