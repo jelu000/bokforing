eee40c5238fff2e7a6e0a9e01a60b3eff02e3c68
Jens L <jelu000@gmail.com>
2019-03-28 00:52:15 +0100

    Nu funkar skatt per kommun, koden måste städas

--- skatt.js
+++ skatt.js
@@ -10,6 +10,9 @@ var data = [];
 var komSkattArray = new Array(); //2D array med all data kummunalskatt
 var kommun_array = new Array();//array med bara kommuner kommunalskatt
 var global_kommun ="";
+
+
+
 //ISO-8859-1 - iso-8859-1
 function getDatan(){
 console.log("getDatan");
@@ -63,7 +66,7 @@ for (var i=0; i<tdata.length; i++){//tdata.length 1346
 }//end of makeKomTaxArray()
 
 function fillOptionkomuner(){
-  tselect = document.getElementById("optionkomuner");
+  tselect = document.getElementById("optionkommuner");
 
   for (var i = 0; i < kommun_array.length; i++) {
         option = document.createElement("option");
@@ -72,4 +75,114 @@ function fillOptionkomuner(){
         option.innerHTML = decodeURIComponent(unescape(tkomun));//unescape(encodeURIComponent(tkomun)); decodeURIComponent(escape(tkomun))
         tselect.appendChild(option);
     }
+}//end of fillOptionkomuner
+
+
+//skriverut skattestsen för vald kommun i selected elementet
+function selectedKommun(){
+  //div id i html-kod
+  var div_html_id = "KomSkattutmatning";
+  //Tömmer Div ellement från gammal visning
+  document.getElementById(div_html_id).innerHTML = "";
+
+  var t_valdkommun_array = new Array();//rader med data för vald kommun
+  var tselectedkommun = document.getElementById("optionkommuner");//.selectedValue;
+
+  //lägger till headings till html table
+  var t_heading = new Array();
+  t_heading[0] = "År";
+  t_heading[1] = "FörsamlingsKod";
+  t_heading[2] = "Kommun";
+  t_heading[3] = "Församling";
+  t_heading[4] = "SummaInkKyrkAvgift";
+  t_heading[5] = "SummaExkKyrkAvgift";
+  t_heading[6] = "KommunalSkatt";
+  t_heading[7] = "LandstingsSkatt";
+  t_heading[8] = "BegravningsAvg";
+  t_heading[9] = "KyrkoAvg";
+
+  t_valdkommun_array.push(t_heading);
+
+  //if (tselectedkommun.selectedIndex == -1)
+    //    return null;
+
+  var tkommun_value =tselectedkommun.options[tselectedkommun.selectedIndex].value;
+
+  for (var i=0; i < komSkattArray.length; i++){
+    var t_row = komSkattArray[i];
+
+    if (tkommun_value == t_row[2]){
+      //console.log(t_row[2]);
+      t_valdkommun_array.push(t_row);
+    }
+
+  }//end of forllop
+
+  createHtmlTable(t_valdkommun_array, div_html_id);
+
+
+}//end selectedkommun()
+
+
+//skriver ut htmltable med selected kommun dataset
+function createHtmlTable(t_array_table, t_divTableName){
+
+  var myTableDiv = document.getElementById(t_divTableName);
+  var t_tableheadings = new Array();
+  t_tableheadings = t_array_table[0];
+
+/*  for (var i=0; i<t_array_table.length; i++){
+    var t_row = t_valdkommun_array[i];
+    console.log(t_row[3]);
+  }*/
+
+  try{
+
+    var table = document.createElement('TABLE')
+    var tableJhaed = document.createElement('THEAD')
+    var tableBody = document.createElement('TBODY')
+
+    //TABLE COLUMNS
+    table.border = '1';
+    table.align = 'center';
+    
+
+    table.appendChild(tableJhaed);
+    var tr = document.createElement('TR');
+    tableJhaed.appendChild(tr);
+
+    for (i = 0; i < t_tableheadings.length; i++) {
+        var th = document.createElement('TH')
+
+        th.appendChild(document.createTextNode(t_tableheadings[i]));
+        tr.appendChild(th);
+    }
+
+    table.appendChild(tableBody);
+    //TABLE ROWS
+
+    for (i = 1; i < t_array_table.length; i++) {
+        var tr = document.createElement('TR');
+        tableBody.appendChild(tr);
+
+        //console.log(i +": ");
+        for (j = 0; j < t_array_table[i].length; j++) {
+
+            var td = document.createElement('TD')
+
+            if (j==1)
+              td.align = 'left';
+
+            td.appendChild(document.createTextNode(t_array_table[i][j]));
+            tr.appendChild(td);
+        }
+        //table.appendChild(tr);
+    }
+
+  myTableDiv.appendChild(table)
+}//end try
+catch(err){
+alert("Fel: " + err);
 }
+
+}//end of function createHtmlKommunTable()
